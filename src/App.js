1244474fb469a4bc14238e42f84b63dec0c403a6
Claire Dranginis <cdranginis@gmail.com>
2017-01-26 11:44:44 -0500

    save events to local storage

--- src/App.js
+++ src/App.js
@@ -14,38 +14,47 @@ class App extends Component {
   }
 
   componentDidMount() {
-    fetch('https://api.eventable.com/v1/token-auth/', {
-      method: 'POST',
-      body: JSON.stringify({
-        'username': 'candidate.5545@eventable.com',
-        'password': 'R8VMaFVK'
-      }),
-      headers: {
-        "Content-Type": "application/json"
-      }
-    })
-    .then(res => res.json())
-    .then(res => {
-      const token = res.token;
-      return fetch('https://api.eventable.com/v1/events/?format=json', {
-        method: 'GET',
+    if (localStorage.getItem('myEvents')) {
+      this.setState({
+        events: JSON.parse(localStorage.getItem('myEvents'))
+      })
+    } else {
+      fetch('https://api.eventable.com/v1/token-auth/', {
+        method: 'POST',
+        body: JSON.stringify({
+          'username': 'candidate.5545@eventable.com',
+          'password': 'R8VMaFVK'
+        }),
         headers: {
-          "Content-Type": "application/json",
-          "Authorization": `Token ${token}`
+          "Content-Type": "application/json"
         }
       })
-    })
-    .then(res => res.json())
-    .then(res => {
-      this.setState({
-        events: res.results
+      .then(res => res.json())
+      .then(res => {
+        const token = res.token;
+        return fetch('https://api.eventable.com/v1/events/?format=json', {
+          method: 'GET',
+          headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Token ${token}`
+          }
+        })
       })
-    });
+      .then(res => res.json())
+      .then(res => {
+        this.setState({
+          events: res.results
+        })
+        localStorage.setItem('myEvents', JSON.stringify(res.results));
+      });
+    }
   }
 
   handleSubmit(newEvent) {
     this.setState({
       events: [...this.state.events, newEvent]
+    }, () => {
+      localStorage.setItem('myEvents', JSON.stringify(this.state.events));
     });
   }
 
